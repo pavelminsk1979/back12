c4c405959e7330b5817ad522d2f4fa872ebba4ad
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-05-03 13:55:56 +0300

    create Array Posts With Info Like Service..

--- src/repositories/blog-query-repository.ts
+++ src/repositories/blog-query-repository.ts
@@ -79,32 +79,6 @@ export const blogQueryRepository = {
         const pagesCount = Math.ceil(totalCount / pageSize)
 
 
-      /*  /!*arrayDocumentsPosts это массив постов type WithId<Post> ,
-        далее достану из каждого обьекта _id(aйдишка поста)
-        буду иметь массив айдишек постов*!/
-        const arrayIdPosts = arrayDocumentsPosts.map(e=>e._id.toString())
-
-
-        /!*далее из коллекции like_post:LikesPostsType
-     достану все документы у которых postId такиеже
-      как в массиве айдишек arrayIdPosts*!/
-        const arrayDocumentsFromLikePostCollection : LikesPostsType[] = await LikesPostsModel.find({
-            postId:{$in:arrayIdPosts}
-        })
-
-
-        /!*создаю массив постов с информацией о лайках и буду его
-    отправлять на фронтенд
-    Создаю items  из  типа
-   PaginationWithOutputPosts<OutputPostWithLikeInfo>
-    *!/
-
-        const arrayPosts:OutputPostWithLikeInfo[] = arrayPostsWithLikeInfoMapper(
-            userId, // для myStatus
-            arrayDocumentsPosts,
-            arrayDocumentsFromLikePostCollection,
-        )
-*/
 
       /*  отдельный сервис для
         создания массива постов с информацией о лайках
